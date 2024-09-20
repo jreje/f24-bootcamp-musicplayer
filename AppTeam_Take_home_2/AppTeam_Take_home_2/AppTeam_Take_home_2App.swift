382285b308e6419b0ea137bdc5ca16577e758c59
John Smith <jreje@ad.unc.edu>
2024-09-20 11:58:18 -0400

    Take Home 2

---
+++ AppTeam_Take_home_2/AppTeam_Take_home_2/AppTeam_Take_home_2App.swift
@@ -0,0 +1,17 @@
+//
+//  AppTeam_Take_home_2App.swift
+//  AppTeam_Take_home_2
+//
+//  Created by Julianne Rejesus  on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct AppTeam_Take_home_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppTeam_Take_home_2/AppTeam_Take_home_2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppTeam_Take_home_2
+//
+//  Created by Julianne Rejesus  on 9/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
